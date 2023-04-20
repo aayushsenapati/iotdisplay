c14a5ac394cee3aa8c1e41398e5d8232b3461aff
Aadithya H Rao <aadithyarao@gmail.com>
2023-04-21 00:25:21 +0530

    material-ui

--- src/App.js
+++ src/App.js
@@ -2,47 +2,68 @@
 import './App.css';
 import axios from "axios";
 import React, { useState } from 'react'
+import '@fontsource/roboto/500.css';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+
+
 function App() {
-  const [ipAddress, setIpAddress] = useState('');
-  const [message, setMessage] = useState('');
-  const handleIpAddressChange = (event) => {
-      setIpAddress(event.target.value);
-  };
-  
-  const handleMessageChange = (event) => {
-      setMessage(event.target.value);
-  };
+    const [ipAddress, setIpAddress] = useState('');
+    const [message, setMessage] = useState('');
+    const handleIpAddressChange = (event) => {
+        setIpAddress(event.target.value);
+    };
 
-  const handleSubmit = () => {
-    if(!ipAddress){
-    axios.get(`http://${ipAddress}`)
-          .then(response => {
-              alert('client connect:',response.data);
-          })
-          .catch(error => {
-              console.log(error);
-          });}
-      axios.get(`http://${ipAddress}/send?message=${message}`)
-          .then(response => {
-              console.log(response.data);
-          })
-          .catch(error => {
-              console.log(error);
-          });
-          
-  };
+    const handleMessageChange = (event) => {
+        setMessage(event.target.value);
+    };
 
-  return (
-      <div>
-          <label htmlFor="ip-address">IP Address:</label>
-          <input id="ip-address" type="text" value={ipAddress} onChange={handleIpAddressChange} />
+    const handleSubmit = () => {
+        if (!ipAddress) {
+            axios.get(`http://${ipAddress}`)
+                .then(response => {
+                    alert('client connect:', response.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+        axios.get(`http://${ipAddress}/send?message=${message}`)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
 
-          <label htmlFor="message">Message:</label>
-          <input id="message" type="text" value={message} onChange={handleMessageChange} />
+    };
 
-          <button onClick={handleSubmit}>Send</button>
-      </div>
-  );
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <div className="form">
+                <div className="inputDiv">
+                    <CssBaseline/>
+                        {/* <label htmlFor="ip-address">IP Address:</label> */}
+                        <TextField InputLabelProps={{ shrink:'true' }} id="outlined-basic" label="IP Address" variant="outlined" value={ipAddress} onChange={handleIpAddressChange} />
+                        {/* <input id="ip-address" type="text" value={ipAddress} onChange={handleIpAddressChange} /> */}
+                        {/* <label htmlFor="message">Message:</label> */}
+                        <TextField InputLabelProps={{ shrink:'true' }} id="outlined-basic" label="PRN" variant="outlined" value={message} onChange={handleMessageChange}/>
+                        {/* <input id="message" type="text" value={message} onChange={handleMessageChange} /> */}
+                </div>
+                <Button  onClick={handleSubmit}>Send</Button>
+            </div>
+        </ThemeProvider>
+    );
 }
 
 export default App;
