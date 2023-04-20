3b72058bd4ceb7bca61bab0b72951c372a6d8d40
aayushsenapati <92628133+aayushsenapati@users.noreply.github.com>
2023-04-20 23:24:33 +0530

    aaaaaaaaa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import axios from "axios";
+import React, { useState } from 'react'
 function App() {
+  const [ipAddress, setIpAddress] = useState('');
+  const [message, setMessage] = useState('');
+  const handleIpAddressChange = (event) => {
+      setIpAddress(event.target.value);
+  };
+  
+  const handleMessageChange = (event) => {
+      setMessage(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    if(!ipAddress){
+    axios.get(`http://${ipAddress}`)
+          .then(response => {
+              alert('client connect:',response.data);
+          })
+          .catch(error => {
+              console.log(error);
+          });}
+      axios.get(`http://${ipAddress}/send?message=${message}`)
+          .then(response => {
+              console.log(response.data);
+          })
+          .catch(error => {
+              console.log(error);
+          });
+          
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <label htmlFor="ip-address">IP Address:</label>
+          <input id="ip-address" type="text" value={ipAddress} onChange={handleIpAddressChange} />
+
+          <label htmlFor="message">Message:</label>
+          <input id="message" type="text" value={message} onChange={handleMessageChange} />
+
+          <button onClick={handleSubmit}>Send</button>
+      </div>
   );
 }
 
